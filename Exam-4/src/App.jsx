5f4a92be4dfd2eed1c7262541e67438a7bd3bd81
Luka0917 <tatuashvililuka@gmail.com>
2025-09-27 12:36:46 +0400

    commit

--- Exam-4/src/App.jsx
+++ Exam-4/src/App.jsx
@@ -23,18 +23,18 @@ function App() {
             <i className={`${theme === 'dark' ? 'text-[#b4c0d4]' : 'text-[#878e99]'} text-[20px] font-[300]`}>Pick a subject to get started.</i>
           </div>
           <div className="flex justify-center items-center flex-col gap-[20px]">
-            <Link to={'/HTML'} className={`${theme === 'dark' ? 'bg-[#3c4d67]' : 'bg-[#B6BBC4]'} flex justify-start items-center gap-[20px] py-[11px] pl-[12px] rounded-[12px] w-[430px] cursor-pointer`}>
+            <Link to={'/'} className={`${theme === 'dark' ? 'bg-[#3c4d67]' : 'bg-[#B6BBC4]'} flex justify-start items-center gap-[20px] py-[11px] pl-[12px] rounded-[12px] w-[430px] cursor-pointer`}>
               <div className="flex justify-center items-center p-[8px] bg-[#ffeee1] rounded-[10px]">
                 <img src="../src/assets/icon-html.svg" alt="" className="pointer-events-none" />
               </div>
               <p className={`${theme === 'dark' ? 'text-[#feffff]' : 'text-[#2d3949]'} text-[20px] font-[500]`}>HTML</p>
             </Link>
-            <Link to={'/CSS'} className={`${theme === 'dark' ? 'bg-[#3c4d67]' : 'bg-[#B6BBC4]'} flex justify-start items-center gap-[20px] py-[11px] pl-[12px] rounded-[12px] w-[430px] cursor-pointer`}>
+            <button className={`${theme === 'dark' ? 'bg-[#3c4d67]' : 'bg-[#B6BBC4]'} flex justify-start items-center gap-[20px] py-[11px] pl-[12px] rounded-[12px] w-[430px] cursor-pointer`}>
               <div className="flex justify-center items-center p-[8px] bg-[#e1fdef] rounded-[10px]">
                 <img src="../src/assets/icon-css.svg" className="pointer-events-none"/>
               </div>
               <p className={`${theme === 'dark' ? 'text-[#feffff]' : 'text-[#2d3949]'} text-[20px] font-[500]`}>CSS</p>
-            </Link>
+            </button>
             <button className={`${theme === 'dark' ? 'bg-[#3c4d67]' : 'bg-[#B6BBC4]'} flex justify-start items-center gap-[20px] py-[11px] pl-[12px] rounded-[12px] w-[430px] cursor-pointer`}>
               <div className="flex justify-center items-center p-[8px] bg-[#edeeff] rounded-[10px]">
                 <img src="../src/assets/icon-js.svg" className="pointer-events-none"/>
